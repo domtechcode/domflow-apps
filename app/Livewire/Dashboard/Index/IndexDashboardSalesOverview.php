bf503c126ba2ae42367c28ccb722eec607214e11
domtechcode <domcorporationtech@gmail.com>
2023-09-26 22:42:05 +0700

    layout & assets

---
+++ app/Livewire/Dashboard/Index/IndexDashboardSalesOverview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Dashboard\Index;
+
+use Livewire\Component;
+
+class IndexDashboardSalesOverview extends Component
+{
+    public function render()
+    {
+        return view('livewire.dashboard.index.index-dashboard-sales-overview');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Dashboard\Index\IndexDashboardSalesOverview;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', IndexDashboardSalesOverview::class);
