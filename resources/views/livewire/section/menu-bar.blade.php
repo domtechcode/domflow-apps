a02d7b81b72aea7ca2e5c187d6be96f300827d95
domtechcode <domcorporationtech@gmail.com>
2023-09-27 04:15:54 +0700

    icon menu

--- resources/views/livewire/section/menu-bar.blade.php
+++ resources/views/livewire/section/menu-bar.blade.php
@@ -35,44 +35,44 @@
             <!-- Products -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-layout-sidebar"></i>
+                    <i class="menu-icon tf-icons ti ti-package"></i>
                     <div data-i18n="Layouts">Products</div>
                 </a>
 
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="layouts-without-menu.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-menu-2"></i>
+                            <i class="menu-icon tf-icons ti ti-packages"></i>
                             <div data-i18n="All Products">All Products</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="../vertical-menu-template/" class="menu-link" target="_blank">
-                            <i class="menu-icon tf-icons ti ti-layout-distribute-vertical"></i>
+                            <i class="menu-icon tf-icons ti ti-file-plus"></i>
                             <div data-i18n="Add Product">Add Product</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="layouts-fluid.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-maximize"></i>
+                            <i class="menu-icon tf-icons ti ti-category"></i>
                             <div data-i18n="Categories">Categories</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="layouts-container.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-arrows-maximize"></i>
+                            <i class="menu-icon tf-icons ti ti-album"></i>
                             <div data-i18n="Container">Units</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="layouts-blank.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-arrows-maximize"></i>
                             <div data-i18n="Blank">Variants</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="layouts-blank.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-shield-check"></i>
                             <div data-i18n="Blank">Warranties</div>
                         </a>
                     </li>
@@ -82,49 +82,49 @@
             <!-- Sales -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-layout-grid-add"></i>
+                    <i class="menu-icon tf-icons ti ti-report-money"></i>
                     <div data-i18n="Apps">Sales</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="app-email.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-mail"></i>
+                            <i class="menu-icon tf-icons ti ti-file-description"></i>
                             <div data-i18n="All Sales">All Sales</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-chat.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-messages"></i>
+                            <i class="menu-icon tf-icons ti ti-file-plus"></i>
                             <div data-i18n="Create Sales">Create Sales</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-calendar.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-calendar"></i>
+                            <i class="menu-icon tf-icons ti ti-notes"></i>
                             <div data-i18n="All Draft">All Draft</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-kanban.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                            <i class="menu-icon tf-icons ti ti-square-plus"></i>
                             <div data-i18n="Create Draft">Create Draft</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-kanban.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                            <i class="menu-icon tf-icons ti ti-file-invoice"></i>
                             <div data-i18n="All Quotations">All Quotations</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-kanban.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                            <i class="menu-icon tf-icons ti ti-file-plus"></i>
                             <div data-i18n="Create Quotation">Create Quotation</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="app-kanban.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                            <i class="menu-icon tf-icons ti ti-truck-delivery"></i>
                             <div data-i18n="Shipments">Shipments</div>
                         </a>
                     </li>
@@ -134,32 +134,32 @@
             <!-- Purchase -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-file"></i>
+                    <i class="menu-icon tf-icons ti ti-file-spreadsheet"></i>
 
                     <div data-i18n="Pages">Purchase</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-description"></i>
                             <div data-i18n="All Purchases">All Purchases</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-plus"></i>
                             <div data-i18n="Create Purchase">Create Purchase</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-symlink"></i>
                             <div data-i18n="All Purchase Returns">All Purchase Returns</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-shredder"></i>
                             <div data-i18n="All Purchase Returns">Create Purchase Returns</div>
                         </a>
                     </li>
@@ -169,37 +169,37 @@
             <!-- Warehouse -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-toggle-left"></i>
+                    <i class="menu-icon tf-icons ti ti-building-warehouse"></i>
                     <div data-i18n="Warehouse">Warehouse</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-receipt"></i>
                             <div data-i18n="Goods Receipt">Goods Receipt</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-package"></i>
                             <div data-i18n="Sales Order Preparation">Sales Order Preparation</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-description"></i>
                             <div data-i18n="All Assets">All Assets</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-file-plus"></i>
                             <div data-i18n="Create Asset">Create Asset</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="modal-examples.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-square"></i>
+                            <i class="menu-icon tf-icons ti ti-arrows-transfer-down"></i>
                             <div data-i18n="Asset Transfer">Asset Transfer</div>
                         </a>
                     </li>
@@ -209,19 +209,19 @@
             <!-- Contacts -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-forms"></i>
+                    <i class="menu-icon tf-icons ti ti-address-book"></i>
                     <div data-i18n="Contacts">Contacts</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="form-validation.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-checkbox"></i>
+                            <i class="menu-icon tf-icons ti ti-users-group"></i>
                             <div data-i18n="Suppliers">Suppliers</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="form-validation.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-checkbox"></i>
+                            <i class="menu-icon tf-icons ti ti-user-star"></i>
                             <div data-i18n="Customers">Customers</div>
                         </a>
                     </li>
@@ -231,14 +231,14 @@
             <!-- Approvals -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-layout-grid"></i>
+                    <i class="menu-icon tf-icons ti ti-browser-check"></i>
                     <div data-i18n="Approvals">Approvals</div>
                 </a>
                 <ul class="menu-sub">
                     <!-- Tables -->
                     <li class="menu-item">
                         <a href="tables-basic.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-table"></i>
+                            <i class="menu-icon tf-icons ti ti-file-check"></i>
                             <div data-i18n="All Approvals">All Approvals</div>
                         </a>
                     </li>
@@ -248,19 +248,19 @@
             <!-- Order Validator -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-chart-bar"></i>
+                    <i class="menu-icon tf-icons ti ti-checkup-list"></i>
                     <div data-i18n="Order Validator">Order Validator</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="maps-leaflet.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-map"></i>
+                            <i class="menu-icon tf-icons ti ti-file-description"></i>
                             <div data-i18n="All Sales Orders">All Sales Orders</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="maps-leaflet.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-map"></i>
+                            <i class="menu-icon tf-icons ti ti-barcode"></i>
                             <div data-i18n="Scan QR Code">Scan QR Code</div>
                         </a>
                     </li>
@@ -270,25 +270,25 @@
             <!-- Settings -->
             <li class="menu-item">
                 <a href="javascript:void(0)" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-chart-bar"></i>
+                    <i class="menu-icon tf-icons ti ti-settings"></i>
                     <div data-i18n="Settings">Settings</div>
                 </a>
                 <ul class="menu-sub">
                     <li class="menu-item">
                         <a href="maps-leaflet.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-map"></i>
+                            <i class="menu-icon tf-icons ti ti-home-cog"></i>
                             <div data-i18n="Business Settings">Business Settings</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="maps-leaflet.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-map"></i>
+                            <i class="menu-icon tf-icons ti ti-map-2"></i>
                             <div data-i18n="Business Location">Business Location</div>
                         </a>
                     </li>
                     <li class="menu-item">
                         <a href="maps-leaflet.html" class="menu-link">
-                            <i class="menu-icon tf-icons ti ti-map"></i>
+                            <i class="menu-icon tf-icons ti ti-receipt-tax"></i>
                             <div data-i18n="Tax Rates">Tax Rates</div>
                         </a>
                     </li>
